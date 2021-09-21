964743b152ccb70f05ef50b0c65d4c2dd1b5d64e
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-21 11:07:48 -0300

    Criando o projeto

---
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/GerenciadorDeSinucaApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoalencar.gerenciador_de_sinuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeSinucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeSinucaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandoalencar/gerenciador_de_sinuca/GerenciadorDeSinucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoalencar.gerenciador_de_sinuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeSinucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
