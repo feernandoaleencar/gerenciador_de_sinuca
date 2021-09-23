9df93412fc8bb7ec291d0f1b189285710bb94321
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 00:26:14 -0300

    Criando metodo de buscar cliente com parâmetro id

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
@@ -2,12 +2,15 @@ package com.fernandoalencar.gerenciador_de_sinuca.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Cliente;
@@ -22,5 +25,15 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
