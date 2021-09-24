0a054426c74b335e3fea3acca02d1fe279559317
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 22:36:10 -0300

    Implementado o metodo de atualizar cliente, com retorno de resposta 200 para sucesso e 404 cliente não encontrado

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
@@ -1,11 +1,7 @@
 package com.fernandoalencar.gerenciador_de_sinuca.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,5 +44,18 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+		
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build(); 
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
 	}
 }
