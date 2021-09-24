dbf3a2426af8672d4397328b450c8744411f083f
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-24 00:10:38 -0300

    Messagens.properties texto fixo para cada tipo de exception e alteração no metodo para retornar as mensagen na classe ApiExceptionHandler

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,9 @@ package com.fernandoalencar.gerenciador_de_sinuca.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource;
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -29,7 +35,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
 			String nome = ((FieldError) error).getField();
-			String mensagem = error.getDefaultMessage();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 			
 			campos.add(new Campo(nome, mensagem));
 			
