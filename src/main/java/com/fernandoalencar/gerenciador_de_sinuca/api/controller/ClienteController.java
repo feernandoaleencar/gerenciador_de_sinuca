1f915e6cd61e00514b14dae7f23f77aa8779df05
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 21:40:26 -0300

    Adicionando anotação @responseStatus(HttpStatus.CREATED) para o metodo adicionar cliente, retorno de requisição 201.

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
@@ -8,12 +8,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Cliente;
@@ -42,6 +44,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
