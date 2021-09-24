28945e90aa26a67f400d0f9cdb0dd64e2687ade4
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 23:36:35 -0300

    Classe Campo para retornar uma mensagem fixada e em qual campo possui a excessão

---
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/exceptionhandler/Campo.java
@@ -0,0 +1,24 @@
+package com.fernandoalencar.gerenciador_de_sinuca.api.exceptionhandler;
+
+public class Campo {
+
+	private String nome;
+	private String mensagem;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+}
