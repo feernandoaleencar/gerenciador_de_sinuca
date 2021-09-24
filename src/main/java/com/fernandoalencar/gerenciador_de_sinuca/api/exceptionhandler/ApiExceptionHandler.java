264b23c5b82c41b0b0db6309a00d410999dcd64f
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 23:16:01 -0300

    Classe para lançamento de exception da API

---
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.fernandoalencar.gerenciador_de_sinuca.api.exceptionhandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+}
