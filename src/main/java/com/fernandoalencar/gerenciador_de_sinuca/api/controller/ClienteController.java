5a02259687e684b4c22fdc3c07560bfa04f95e14
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-23 22:49:10 -0300

    Implementado o metodo de excluir cliente, com retorno de resposta 204 deu sucesso só que não tem corpo no retorno e 404 cliente não encontrado

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,17 @@ public class ClienteController {
 		cliente = clienteRepository.save(cliente);
 		
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
