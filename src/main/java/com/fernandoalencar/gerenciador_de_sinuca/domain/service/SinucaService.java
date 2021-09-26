b9ad24838ff7d664810bc6da95df3d47aa18b718
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-09-26 13:08:10 -0300

    Criado o método de adicionar sinuca e buscando atributos do cliente com tratamento de resposta caso não localize o cliente

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/service/SinucaService.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/service/SinucaService.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fernandoalencar.gerenciador_de_sinuca.domain.exception.NegocioException;
+import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Cliente;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Sinuca;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.StatusSinuca;
+import com.fernandoalencar.gerenciador_de_sinuca.domain.repository.ClienteRepository;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.repository.SinucaRepository;
 
 @Service
@@ -15,8 +18,14 @@ public class SinucaService {
 	@Autowired
 	private SinucaRepository sinucaRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public Sinuca criar(Sinuca sinuca) {
+		Cliente cliente = clienteRepository.findById(sinuca.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
 		
+		sinuca.setCliente(cliente);
 		sinuca.setStatus(StatusSinuca.DISPONIVEL);
 		sinuca.setDataAbertura(LocalDateTime.now());
 		
