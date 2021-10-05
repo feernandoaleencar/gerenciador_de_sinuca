3ec44f9140b2f74c8765cd1d34165ace6084296b
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-04 22:53:14 -0300

    Classe clienteResumoModel com apenas alguns atributos da classe cliente.

---
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/model/ClienteResumoModal.java
@@ -0,0 +1,42 @@
+package com.fernandoalencar.gerenciador_de_sinuca.api.model;
+
+public class ClienteResumoModal {
+
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
