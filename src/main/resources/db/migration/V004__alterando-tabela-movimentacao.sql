f46b0a0477017ae98094f61aa7bb9bd5a47a9d04
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-12 20:54:26 -0300

    Deploy heroku com hibernete 10

--- src/main/resources/db/migration/V004__alterando-tabela-movimentacao.sql
+++ src/main/resources/db/migration/V004__alterando-tabela-movimentacao.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `movimentacao` 
-CHANGE COLUMN `total_fichas_empresa` `total_fichas_empresa` BIGINT NULL ,
-CHANGE COLUMN `total_fichas_cliente` `total_fichas_cliente` BIGINT NULL ,
-CHANGE COLUMN `diferenca_Fichas` `diferenca_Fichas` BIGINT NULL ,
-CHANGE COLUMN `total_final_fichas` `total_final_fichas` BIGINT NULL ;
+ALTER TABLE movimentacao
+CHANGE COLUMN total_fichas_empresa total_fichas_empresa BIGINT NULL ,
+CHANGE COLUMN total_fichas_cliente total_fichas_cliente BIGINT NULL ,
+CHANGE COLUMN diferenca_Fichas diferenca_Fichas BIGINT NULL ,
+CHANGE COLUMN total_final_fichas total_final_fichas BIGINT NULL ;
