4610a227cc6b42286d7488171a40516c63efe24a
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-11-23 23:31:09 -0300

    Adicionando a classe SecurityConfig a autenticação Basic

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/core/SecurityConfig.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/core/SecurityConfig.java
@@ -1,10 +1,29 @@
 package com.fernandoalencar.gerenciador_de_sinuca.core;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ROLE");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .csrf().disable();
+    }
 }
