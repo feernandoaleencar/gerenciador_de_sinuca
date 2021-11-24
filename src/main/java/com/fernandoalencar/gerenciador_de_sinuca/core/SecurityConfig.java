3722c475c09965bbabfd36d5d1539fd4651bba6d
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-11-23 23:29:33 -0300

    Adicionando a classe SecurityConfig e dependency spring security ao pom.xml

---
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/core/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.fernandoalencar.gerenciador_de_sinuca.core;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
