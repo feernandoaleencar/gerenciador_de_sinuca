86e9e2d3645e3d23c58fb88d15d1c3a2fc4e4aa8
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-04-23 23:20:59 -0300

    - Adicionando páginação para listar clientes

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/ClienteController.java
@@ -6,17 +6,11 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Cliente;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.repository.ClienteRepository;
@@ -36,8 +30,8 @@ public class ClienteController {
 	
 	//Método listar clientes
 	@GetMapping
-	public List<Cliente> listar(){
-		return clienteRepository.findAll();
+	public Page<Cliente> listar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable){
+		return clienteRepository.findByNomeContaining(nome, pageable);
 	}
 	
 	
--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/repository/ClienteRepository.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.fernandoalencar.gerenciador_de_sinuca.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	
 	List<Cliente> findByNome(String nome);
-	List<Cliente> findByNomeContaining(String nome);
+	Page<Cliente> findByNomeContaining(String nome, Pageable pageable);
 	Cliente findByEmail(String email);
 	Cliente findByCpf(String cpf);
 	
