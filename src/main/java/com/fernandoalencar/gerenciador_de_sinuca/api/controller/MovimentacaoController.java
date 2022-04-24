578a446ec86bbf85c71c462c014354c1e0673308
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-04-24 14:20:42 -0300

    - Adicionando páginação para listar movimentações de sinuca

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/MovimentacaoController.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/api/controller/MovimentacaoController.java
@@ -1,48 +1,46 @@
 package com.fernandoalencar.gerenciador_de_sinuca.api.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fernandoalencar.gerenciador_de_sinuca.api.dto.MovimentacaoInputModel;
 import com.fernandoalencar.gerenciador_de_sinuca.api.dto.MovimentacaoModel;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.exception.EntidadeNaoEncontradaException;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Movimentacao;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Sinuca;
+import com.fernandoalencar.gerenciador_de_sinuca.domain.repository.MovimentacaoRepository;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.repository.SinucaRepository;
 import com.fernandoalencar.gerenciador_de_sinuca.domain.service.SinucaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sinucas/{sinucaId}/movimentacao")
 public class MovimentacaoController {
-	
+
+	@Autowired
+	private SinucaRepository sinucaRepository;
+
+	@Autowired
+	private MovimentacaoRepository movimentacaoRepository;
+
 	@Autowired
 	private SinucaService sinucaService;
 	
 	@Autowired
 	private ModelMapper modelMapper;
 	
-	@Autowired
-	private SinucaRepository sinucaRepository;
-	
 	@GetMapping
-	public List<MovimentacaoModel> listar(@PathVariable Long sinucaId){
-		
+	public Page<Movimentacao> listar(@PathVariable Long sinucaId, Pageable pageable){
+
 		Sinuca sinuca = sinucaRepository.findById(sinucaId).orElseThrow(() -> new EntidadeNaoEncontradaException("Sinuca não encontrada"));
-		
-		return toCollectionModel(sinuca.getMovimentacoes());
+
+		return movimentacaoRepository.findBySinuca(sinuca, pageable);
 	}
 	
 	@PostMapping
--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/repository/MovimentacaoRepository.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/domain/repository/MovimentacaoRepository.java
@@ -1,11 +1,15 @@
 package com.fernandoalencar.gerenciador_de_sinuca.domain.repository;
 
+import com.fernandoalencar.gerenciador_de_sinuca.api.dto.MovimentacaoModel;
+import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Movimentacao;
+import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Sinuca;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.fernandoalencar.gerenciador_de_sinuca.domain.model.Movimentacao;
 
 @Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
 
+    Page<Movimentacao> findBySinuca(Sinuca sinuca, Pageable pageable);
 }
