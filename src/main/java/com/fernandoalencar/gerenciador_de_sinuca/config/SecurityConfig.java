f93d034706c055db01f4aa811009a34d92edeaee
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-04-25 21:49:30 -0300

    - Implementando autenticação Basic

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/SecurityConfig.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.fernandoalencar.gerenciador_de_sinuca.core;
+package com.fernandoalencar.gerenciador_de_sinuca.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
