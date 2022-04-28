5082727fcd3bd5decbf90c37189ae5fe0744c917
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-04-27 23:31:19 -0300

    - Renovando o access token com o refresh token

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -31,21 +30,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret(passwordEncoder.encode("@ngul@r0"))
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(20)
+                .refreshTokenValiditySeconds(3600 * 24);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
                 .accessTokenConverter(accessTokenConverter())
-                .authenticationManager(authenticationManager);
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("permitAll()");
+                .tokenStore(tokenStore())
+                .reuseRefreshTokens(false);
     }
 
     @Bean
