8a3ee9f36ed99237c4d209493d584e60c8df6964
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-05-02 23:23:36 -0300

    - Preparando o access token com o refresh token

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
@@ -29,6 +29,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(1800)
+                .refreshTokenValiditySeconds(3600 * 24).and()
+                .withClient("mobile") //
+                .secret("mob!le") //
+                .scopes("read") //
+                .authorizedGrantTypes("password", "refresh_token") //
+                .accessTokenValiditySeconds(1800) //
                 .refreshTokenValiditySeconds(3600 * 24);
     }
 
