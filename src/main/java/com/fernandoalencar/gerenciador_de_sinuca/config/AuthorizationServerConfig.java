b972896966ddf4007896948b71d31806d6f9fc54
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-05-06 23:33:28 -0300

    - Preparando o access token com o refresh token

--- src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
+++ src/main/java/com/fernandoalencar/gerenciador_de_sinuca/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -21,21 +22,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("angular")
-                .secret("@ngul@r0")
+                .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(1800)
-                .refreshTokenValiditySeconds(3600 * 24)
-                .and()
-                .withClient("mobile") //
-                .secret("mob!le") //
-                .scopes("read", "write") //
-                .authorizedGrantTypes("password", "refresh_token") //
-                .accessTokenValiditySeconds(1800) //
                 .refreshTokenValiditySeconds(3600 * 24);
     }
 
@@ -51,6 +48,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+
         accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
 
         return accessTokenConverter;
